9109ad6a23c71200ed69dfab084f9bcc7849c897
Fahad Hasan <44fahadhasan@gmail.com>
2024-11-17 09:26:09 +0600

    api updated

--- controllers/apartmentController.js
+++ controllers/apartmentController.js
@@ -34,9 +34,9 @@ exports.getAllApartments = async (req, res) => {
     }
 
     // search by division or district
-    if (division) {
+    if (division && country.toLowerCase() !== "all") {
       query["address.divisionOrThana"] = division.toLowerCase();
-    } else if (districts) {
+    } else if (districts && country.toLowerCase() !== "all") {
       query["address.divisionOrThana"] = districts.toLowerCase();
     }
 

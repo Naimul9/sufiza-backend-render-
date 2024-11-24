6909b6314cc8c1294893ce40b99fc9adf6668e9a
Fahad Hasan <44fahadhasan@gmail.com>
2024-11-24 10:46:52 +0600

    apartment model upd

--- models/Apartment.js
+++ models/Apartment.js
@@ -115,7 +115,7 @@ const apartmentSchema = new mongoose.Schema(
       type: String,
       lowercase: true,
       enum: {
-        values: ["north facing", "south facing", "east facing", "west facing"],
+        values: ["north", "south", "east", "west"],
         message:
           "Orientation must be 'north facing', 'south facing', 'east facing', or 'west facing'",
       },
